54a0a055536f3482b837eabe09dfe7c6d139afd1
Gleb Dovzhenko <dovjenko.g@gmail.com>
2019-12-01 11:42:59 -0800

    Updated build_win.cmd

--- build_win.cmd
+++ build_win.cmd
@@ -2,5 +2,6 @@ rmdir build
 rmdir dist
 del P61ViewerMain.spec
 del apps\P61Viewer.exe
-pyinstaller -i img\icon.ico src\P61ViewerMain.py --windowed --onefile
-xcopy dist\P61ViewerMain.exe apps\\P61Viewer.exe
+pyinstaller src\P61ViewerMain.py --windowed --onefile --paths C:\Users\glebdovzhenko\Anaconda3\envs\P61Viewer6\Lib\site-packages\scipy\.libs
+echo f|xcopy dist\P61ViewerMain.exe apps\P61Viewer.exe
+apps\P61Viewer.exe
