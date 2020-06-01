a7aef253f26fbeb3c36efbf700b5b5c7e2d823f9
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-06-01 15:32:12 +0200

    Updated FloatEditWidget to be more universal

--- src/FitWidgets/FloatEditWidget.py
+++ src/FitWidgets/FloatEditWidget.py
@@ -1,18 +1,22 @@
 from PyQt5.QtWidgets import QLineEdit
 import re
 import numpy as np
+from collections import defaultdict
 
 
 class FloatEditWidget(QLineEdit):
     """"""
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, inf_allowed=True, none_allowed=False, init_val=0.):
         QLineEdit.__init__(self, parent=parent)
 
-        self.float_regexp = re.compile(
-            r'^(?P<main>-?[0-9]+\.?)(?P<decimal>[0-9]+)?([Ee](?P<exp>[+-]{0,1}[0-9]+))?$|'
-            r'^(?P<inf>\s*INF|inf|-INF|-inf\s*)$'
-        )
-        self._value = 0.
+        rgx = r'^(?P<main>-?[0-9]+\.?)(?P<decimal>[0-9]+)?([Ee](?P<exp>[+-]{0,1}[0-9]+))?$'
+        if inf_allowed:
+            rgx += r'|^(?P<inf>\s*INF|inf|-INF|-inf\s*)$'
+        if none_allowed:
+            rgx += r'|^(?P<none>\s*None|none\s*)$'
+
+        self.float_regexp = re.compile(rgx)
+        self._value = init_val
 
         self.textChanged.connect(self.on_text_changed)
         self.returnPressed.connect(self.on_text_submitted)
@@ -30,9 +34,12 @@ class FloatEditWidget(QLineEdit):
     def on_text_submitted(self):
         match = self.on_text_changed()
         if match:
-            gd = match.groupdict()
+            gd = defaultdict(lambda *args: None, match.groupdict())
+
             if gd['inf'] is not None:
                 self._value = np.float(gd['inf'])
+            elif gd['none'] is not None:
+                self._value = None
             elif gd['main'] is not None:
                 self._value = float(gd['main'])
                 if gd['decimal'] is not None:
@@ -40,17 +47,23 @@ class FloatEditWidget(QLineEdit):
                                     else -float(gd['decimal'])) * 10 ** (-len(gd['decimal']))
                 if gd['exp'] is not None:
                     self._value *= 10 ** (int(gd['exp']))
-            self.setText('%.03E' % self._value)
+            if self._value is not None:
+                self.setText('%.03E' % self._value)
+            else:
+                self.setText('None')
 
     def get_value(self):
         return self._value
 
     def set_value(self, val):
-        if not isinstance(val, (int, float, np.int, np.float)):
-            raise ValueError(val, 'is not any of the', (int, float, np.int, np.float), 'types')
+        if not isinstance(val, (int, float, np.int, np.float, type(None))):
+            raise ValueError(val, 'is not any of the', (int, float, np.int, np.float, type(None)), 'types')
         else:
             self._value = val
-            self.setText('%.03E' % self._value)
+            if self._value is not None:
+                self.setText('%.03E' % self._value)
+            else:
+                self.setText('None')
 
     value = property(fget=get_value, fset=set_value)
 
