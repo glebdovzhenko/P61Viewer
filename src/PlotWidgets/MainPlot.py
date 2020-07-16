363f6ad5971cdd11a369bef95936bd76f9d22d52
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-07-16 16:56:59 +0200

    Switched the MainPlot widget's plotting library from matplotlib to qtgraph

--- src/PlotWidgets/MainPlot.py
+++ src/PlotWidgets/MainPlot.py
@@ -1,8 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
+import pyqtgraph as pg
 
 from P61App import P61App
 
@@ -11,53 +8,50 @@ class MainPlot(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent=parent)
         self.q_app = P61App.instance()
+        pg.setConfigOptions(antialias=True)
+        pg.setConfigOption('background', 'w')
 
-        line_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        self._line_ax = line_canvas.figure.subplots()
-        self._line_ax.set_xlabel('Energy, [keV]')
-        self._line_ax.set_ylabel('Intensity, [counts]')
+        graph_widget = pg.GraphicsLayoutWidget(show=True)
+        self._line_ax = graph_widget.addPlot(title="Imported spectra")
+        self._line_ax.setLabel('bottom', "Energy", units='eV')
+        self._line_ax.setLabel('left', "Intensity", units='counts')
+        self._line_ax.showGrid(x=True, y=True)
+        self._lines = []
 
         layout = QVBoxLayout()
         self.setLayout(layout)
-        layout.addWidget(line_canvas)
-        layout.addWidget(NavigationToolbar(line_canvas, self))
+        layout.addWidget(graph_widget)
 
         self.q_app.dataRowsInserted.connect(self.on_data_rows_appended)
         self.q_app.dataRowsRemoved.connect(self.on_data_rows_removed)
         self.q_app.dataActiveChanged.connect(self.on_data_active_changed)
 
     def on_data_rows_appended(self, pos, n_rows):
+        self._lines = self._lines[:pos] + [None] * n_rows + self._lines[pos:]
         for ii in range(pos, pos + n_rows):
             data = self.q_app.data.loc[ii, ['DataX', 'DataY', 'Color']]
-            self._line_ax.plot(data['DataX'], data['DataY'], color=str(hex(data['Color'])).replace('0x', '#'))
-        self.update_line_axes()
+            self._lines[ii] = self._line_ax.plot(1E3 * data['DataX'], data['DataY'],
+                                                 pen=str(hex(data['Color'])).replace('0x', '#'))
 
     def on_data_rows_removed(self, rows):
         for ii in sorted(rows, reverse=True):
-            self._line_ax.lines[ii].remove()
-        self.update_line_axes(autoscale=False)
+            self._line_ax.removeItem(self._lines[ii])
+            self._lines.pop(ii)
 
     def on_data_active_changed(self, rows):
         for ii in rows:
             if self.q_app.data.loc[ii, 'Active']:
-                self._line_ax.lines[ii].set_linestyle('-')
+                self._lines[ii].setPen(str(hex(self.q_app.data.loc[ii, 'Color'])).replace('0x', '#'))
             else:
-                self._line_ax.lines[ii].set_linestyle('')
-        self.update_line_axes(autoscale=False)
-
-    def update_line_axes(self, autoscale=True):
-        if autoscale:
-            self._line_ax.autoscale()
-            self._line_ax.figure.canvas.toolbar.update()
-        self._line_ax.figure.canvas.draw()
+                self._lines[ii].setPen(None)
 
 
 if __name__ == '__main__':
-    from ListWidgets import DataSetManager
+    from DatasetManager import DatasetManager
     import sys
     q_app = P61App(sys.argv)
     app = MainPlot()
-    app2 = DataSetManager()
+    app2 = DatasetManager()
     app.show()
     app2.show()
     sys.exit(q_app.exec_())
