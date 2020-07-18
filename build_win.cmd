21b0d661587225ab07dc93fb369b64d2be705767
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-07-18 10:35:45 -0700

    added a hidden import

--- build_win.cmd
+++ build_win.cmd
@@ -2,6 +2,6 @@ rmdir build
 rmdir dist
 del P61ViewerMain.spec
 del apps\P61Viewer.exe
-pyinstaller src\P61ViewerMain.py --windowed --onefile --paths C:\Users\glebdovzhenko\Anaconda3\envs\P61Viewer6\Lib\site-packages\scipy\.libs
+pyinstaller src\P61ViewerMain.py --windowed --onefile --paths C:\Users\glebdovzhenko\Anaconda3\envs\P61Viewer6\Lib\site-packages\scipy\.libs --hidden-import=pyopengl
 echo f|xcopy dist\P61ViewerMain.exe apps\P61Viewer.exe
 apps\P61Viewer.exe
