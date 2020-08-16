8e2e8dcb83d34cc3ae2880a4907250de310bd6c8
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-08-16 06:12:43 -0700

    Removed an extra fit widget

--- src/PeakTrackerWidgets/PeakTrackerWidget.py
+++ src/PeakTrackerWidgets/PeakTrackerWidget.py
@@ -1,9 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout, QTabWidget
+from PyQt5.QtWidgets import QWidget, QGridLayout
 
 from P61App import P61App
 
 from PeakTrackerWidgets.AutoFind import AutoFindWidget
-from PeakTrackerWidgets.PeakFit import PeakFitWidget
 from DatasetManager import DatasetViewer
 from PlotWidgets import PTPlot3D, PTPlot3DWidget
 
@@ -13,17 +12,13 @@ class PeakAnalysisWidget(QWidget):
         QWidget.__init__(self, parent=parent)
         self.q_app = P61App.instance()
 
-        self.tabs = QTabWidget(parent=self)
         self.afw = AutoFindWidget(parent=self)
-        self.fit = PeakFitWidget(parent=self)
-        self.tabs.addTab(self.afw, 'Search')
-        self.tabs.addTab(self.fit, 'Fit')
         self.list = DatasetViewer(parent=self)
         self.plot = PTPlot3DWidget(PTPlot3D(parent=self), parent=self)
 
         layout = QGridLayout()
         self.setLayout(layout)
-        layout.addWidget(self.tabs, 1, 1, 1, 1)
+        layout.addWidget(self.afw, 1, 1, 1, 1)
         layout.addWidget(self.list, 2, 1, 1, 1)
         layout.addWidget(self.plot, 1, 2, 2, 1)
         layout.setColumnStretch(1, 1)
