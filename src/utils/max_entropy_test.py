d81de79647b0c5c6f68bef07284944482adc2f82
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-10-07 17:27:01 +0200

    upd on the max entropy method test

--- src/utils/max_entropy_test.py
+++ src/utils/max_entropy_test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import os
+import torch
 
 
 def plot_prep(x):
@@ -10,35 +11,62 @@ def plot_prep(x):
             'marker': '.', 'linestyle': ''}
 
 
-def max_entropy_method(observed, weights):
+def max_entropy_method(observed, weights, delta):
     """
     :param observed:
     :return:
 
-    Entropy:
-        S = -1. * np.sum(mem_predicted * np.log(mem_predicted))
-
-    Constraints:
-        0: np.sum(mem_predicted) - np.sum(observed) = 0
-        1: np.mean(weights * (observed - mem_predicted) ** 2) - 1 = 0
-
-    Calculation:
-        mem_predicted = np.exp(
-            - lambdas[0]
-            - lambdas[1] * 2. * weights * (observed - mem_predicted)  / observed.shape[0]
-            )
     """
+
+    mem_predicted = torch.tensor(observed, requires_grad=True)
+    observed = torch.tensor(observed)
+    weights = torch.tensor(weights)
+    lambdas = torch.tensor([1., 1., 1.], requires_grad=True)
+
+    optimizer = torch.optim.Adam([mem_predicted, lambdas], lr=1E-3)
+    for i in range(30000):
+        optimizer.zero_grad()
+
+        tmp = 0.5 * (torch.cat((mem_predicted[1:], torch.tensor([0.], dtype=torch.double))) + torch.cat((torch.tensor([0.], dtype=torch.double), mem_predicted[:-1])))
+
+        metric = torch.sum(torch.abs(
+            torch.log(mem_predicted) + 1. +
+            lambdas[0] * mem_predicted +
+            lambdas[1] * 2. * weights * (mem_predicted - observed) / observed.shape[0] +
+            lambdas[2] * 2. * weights * (mem_predicted - tmp) / observed.shape[0]
+        ))
+        metric += torch.abs(
+            torch.sum(mem_predicted) - torch.sum(observed)
+        )
+        metric += torch.abs(
+            torch.mean(weights * (mem_predicted - observed) ** 2) - 1.
+        )
+        metric += torch.abs(
+            torch.mean(weights * (mem_predicted - tmp) ** 2) - delta
+        )
+
+        metric.backward()
+        optimizer.step()
+
+        if (i + 1) % 1000 == 0:
+            with torch.no_grad():
+                print(i + 1, lambdas, metric, torch.sum(mem_predicted), torch.sum(observed))
+
+    return mem_predicted.detach().numpy()
 
 
 if __name__ == '__main__':
     wd = os.path.join('..', '..', 'test_files', 'generated', 'probe_5-5_sin2psi')
 
     for f_name in os.listdir(wd):
-        dd = pd.read_csv(os.path.join(wd, f_name), header=0, index_col='eV')
+        dd = pd.read_csv(os.path.join(wd, f_name), header=0, index_col='eV', dtype=np.float)
 
         dd = dd[62000:65000]
-        plt.errorbar(**plot_prep(dd))
-        plt.show()
+        xx, yy = np.array(dd.index).flatten(), np.array(dd.values).flatten()
+        xx, yy = xx[(~np.isnan(yy)) & (yy > 0)], yy[(~np.isnan(yy)) & (yy > 0)]
 
-        max_entropy_method(dd.values, np.array([dd.shape[0] / dd.sum()] * dd.shape[0]))
-        break
+        predicted = max_entropy_method(yy, np.sqrt(yy), 0.1)
+
+        plt.plot(xx, yy, '.-')
+        plt.plot(xx, predicted)
+        plt.show()
