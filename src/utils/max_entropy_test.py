8e37833b2aee77f954b596162d6fb3fc81096d88
Gleb Dovzhenko <dovjenko.g@gmail.com>
2020-12-04 21:30:55 +0100

    Example in working order

--- src/utils/max_entropy_test.py
+++ src/utils/max_entropy_test.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 import torch
+import h5py
 
 
 def plot_prep(x):
@@ -30,6 +31,14 @@ def t_c2(pp, ww):
     return torch.mean(ww[:-1] * (pp[:-1] - pp[1:]) ** 2)
 
 
+def project(t1, t2):
+    return t2 * torch.dot(t1, t2) / torch.sum(t2 ** 2)
+
+
+def reject(t1, t2):
+    return t1 - project(t1, t2)
+
+
 def maximize_entropy(observed: np.ndarray, weights: np.ndarray):
     """"""
     observed = torch.tensor(observed, requires_grad=False)
@@ -40,7 +49,7 @@ def maximize_entropy(observed: np.ndarray, weights: np.ndarray):
 
     lr = 1E-2
     chisqr_window = 1E-2
-    for ii in range(30000):
+    for ii in range(5000):
 
         p_s, p_c0, p_c1, p_c2 = (predicted.detach().clone().requires_grad_(True),
                                  predicted.detach().clone().requires_grad_(True),
@@ -52,27 +61,20 @@ def maximize_entropy(observed: np.ndarray, weights: np.ndarray):
         grad = p_s.grad
 
         c2 = t_c2(p_c2, weights)
-        if c2.data > 0.3:
-            c2.backward()
-            c2_grad = p_c2.grad
-            if torch.norm(c2_grad) > 0:
-                c2_grad = c2_grad / torch.norm(c2_grad)
-                grad = c2_grad * torch.dot(grad, c2_grad)
+        c2.backward()
+        if torch.norm(p_c2.grad) > 0:
+            grad = project(grad, p_c2.grad)
 
         c0 = t_c0(p_c0)
         c0.backward()
-        c0_grad = p_c0.grad
-        if torch.norm(c0_grad) > 0:
-            c0_grad = c0_grad / torch.norm(c0_grad)
-            grad = grad - c0_grad * torch.dot(grad, c0_grad)
+        if torch.norm(p_c0.grad) > 0:
+            grad = reject(grad, p_c0.grad)
 
         c1 = t_c1(p_c1, observed, weights)
+        c1.backward()
         if 1. - chisqr_window <= c1.data <= 1. + chisqr_window:
-            c1.backward()
-            c1_grad = p_c1.grad
-            if torch.norm(c1_grad) > 0:
-                c1_grad = c1_grad / torch.norm(c1_grad)
-                grad = grad - c1_grad * torch.dot(grad, c1_grad)
+            if torch.norm(p_c1.grad) > 0:
+                grad = reject(grad, p_c1.grad)
 
         track['S'].append(-ent.data)
         track['c0'].append(c0.data)
@@ -80,24 +82,25 @@ def maximize_entropy(observed: np.ndarray, weights: np.ndarray):
         track['c2'].append(c2.data)
 
         predicted -= lr * grad
-        print("%d :: S=%f : c0=%f : c1=%f : c2=%f" % (ii, track['S'][-1], track['c0'][-1],
-                                                      track['c1'][-1], track['c2'][-1]))
+        if ii % 100 == 0:
+            print("%d :: S=%f : Sum=%f : chisqr=%f : E[dy/dx]=%f" %
+                  (ii, track['S'][-1], track['c0'][-1], track['c1'][-1], track['c2'][-1]))
 
-    plt.figure()
-    ax = plt.subplot(411)
-    plt.title('Entropy maximization track')
-    plt.plot(track['S'], label='Entropy')
-    plt.legend()
-    plt.subplot(412, sharex=ax)
-    plt.plot(track['c0'], label='Total count')
-    plt.legend()
-    plt.subplot(413, sharex=ax)
-    plt.plot(track['c1'], label='$\chi^{2}$')
-    plt.legend()
-    plt.subplot(414, sharex=ax)
-    plt.plot(track['c2'], label='E[dy/dx]')
-    plt.legend()
-    plt.show()
+    # plt.figure()
+    # ax = plt.subplot(411)
+    # plt.title('Entropy maximization track')
+    # plt.plot(track['S'], label='Entropy')
+    # plt.legend()
+    # plt.subplot(412, sharex=ax)
+    # plt.plot(track['c0'], label='Total count')
+    # plt.legend()
+    # plt.subplot(413, sharex=ax)
+    # plt.plot(track['c1'], label='$\chi^{2}$')
+    # plt.legend()
+    # plt.subplot(414, sharex=ax)
+    # plt.plot(track['c2'], label='E[dy/dx]')
+    # plt.legend()
+    # plt.show()
 
     return predicted.detach().data
 
@@ -105,15 +108,24 @@ def maximize_entropy(observed: np.ndarray, weights: np.ndarray):
 if __name__ == '__main__':
     wd = os.path.join('..', '..', 'test_files', 'generated', 'probe_5-5_sin2psi')
 
-    for f_name in os.listdir(wd):
+    for f_name in filter(lambda x: x[-4:] == '.csv', os.listdir(wd)):
         dd = pd.read_csv(os.path.join(wd, f_name), header=0, index_col='eV', dtype=np.float)
 
         # dd = dd[62000:65000]
         xx, yy = np.array(dd.index).flatten(), np.array(dd.values).flatten()
-        xx, yy = xx[(~np.isnan(yy)) & (yy > 0)], yy[(~np.isnan(yy)) & (yy > 0)]
+        xx_, yy = xx[(~np.isnan(yy)) & (yy > 0)], yy[(~np.isnan(yy)) & (yy > 0)]
 
         yy_opt = maximize_entropy(yy, np.ones(shape=yy.shape) * yy.shape[0] / np.sum(yy))
-
-        plt.plot(xx, yy, linestyle='', marker='.')
-        plt.plot(xx, yy_opt, linestyle='--', marker='')
+        yy_opt = np.interp(xx, xx_, yy_opt)
+        plt.plot(xx_ * 1E-3, yy, linestyle='', marker='.', label='Observed')
+        plt.plot(xx * 1E-3, yy_opt, linestyle='--', marker='', label='MEM optimized')
+        plt.xlabel('Energy, keV')
+        plt.ylabel('Intensity, counts')
+        plt.legend()
         plt.show()
+
+        # d = pd.DataFrame({'x': xx * 1E-3, 'y': yy_opt})
+        # d.set_index('x', inplace=True)
+        # with open(os.path.join(wd, f_name.replace('.csv', '.xy')), 'w') as f:
+        #     f.write('# Custom header\n' + '# keV counts\n')
+        #     d.to_csv(f, sep=' ', header=False)
